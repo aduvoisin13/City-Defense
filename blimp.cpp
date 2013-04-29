89c19eaf636fbd2fecc36373a6b70e1e3219ae87
Andre Duvoisin <duvoisin@usc.edu>
2013-04-28 23:01:42 -0700

    Game speeds up correctly at intervals. close #13

--- blimp.cpp
+++ blimp.cpp
@@ -1,17 +1,51 @@
 #include "blimp.h"
 
+/** Default constructor.
+ *
+ * @return Nothing.
+ */
 Blimp::Blimp() {
 	shotsToKill = 1;
 }
 
+/** Moves the blimp.
+ *
+ * @param speedMult The speed multiplier; increases as the game progresses.
+ * @return Nothing.
+ */
 void Blimp::move(int speedMult) {
 	moveBy(3 * speedMult, 0);
 }
 
-void Blimp::animate(int timer) { }
+/** Animates the blimp. (Required Function)
+ *
+ * @param time The current time of the timer; used to determine animation.
+ * @return Nothing.
+ */
+void Blimp::animate(int timer) {
+	timer++; // Needed for compiler.
+}
 
+/** Returns if the blimp is dead or not.
+ *
+ * @return True if dead, false if not.
+ */
 bool Blimp::isDead() { return (shotsToKill == 0); }
+
+/** Decreases the shots to kill the blimp.
+ *
+ * @return Nothing.
+ */
 void Blimp::decreaseShotsToKill() { shotsToKill--; }
 
+/** Returns the number of points that this blimp is worth when hit.
+ *
+ * @return The increase in score that the player gets for hitting the blimp.
+ */
 int Blimp::scoreHit() { return 0; }
+
+/** Returns the number of points that this blimp is worth when destroyed.
+ *
+ * @return The increase in score that the player gets for destroying the blimp.
+ */
 int Blimp::scoreKilled() { return 1000; }
--- blimp.h
+++ blimp.h
@@ -3,16 +3,24 @@
 
 #include "thing.h"
 
+/** Class that handles every instance of
+ * the blimp. Inherits from Thing. */
 class Blimp : public Thing {
 	public:
+	/** Default constructor. */
 	Blimp();
+	/** Move the blimp. */
 	void move(int speedMult);
+	/** Animates the blimp's sprite. */
 	void animate(int);
+	/** Is the blimp dead? */
 	bool isDead();
+	/** Decreases the shots to kill the blimp by one. */
 	void decreaseShotsToKill();
+	/** Increases the score when the blimp is hit. */
 	int scoreHit();
+	/** Increases the score when the blimp is killed. */
 	int scoreKilled();
-	private:
 };
 
 #endif
