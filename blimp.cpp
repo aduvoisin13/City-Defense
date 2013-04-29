3af2b176e583d0bba4ad83cd0a762ad34769b192
Andre Duvoisin <duvoisin@usc.edu>
2013-04-28 21:04:12 -0700

    Blimp works correctly and travels across screen. close #10

--- blimp.cpp
+++ blimp.cpp
@@ -1,1 +1,17 @@
 #include "blimp.h"
+
+Blimp::Blimp() {
+	shotsToKill = 1;
+}
+
+void Blimp::move(int speedMult) {
+	moveBy(3 * speedMult, 0);
+}
+
+void Blimp::animate(int timer) { }
+
+bool Blimp::isDead() { return (shotsToKill == 0); }
+void Blimp::decreaseShotsToKill() { shotsToKill--; }
+
+int Blimp::scoreHit() { return 0; }
+int Blimp::scoreKilled() { return 1000; }
--- blimp.h
+++ blimp.h
@@ -4,6 +4,15 @@
 #include "thing.h"
 
 class Blimp : public Thing {
+	public:
+	Blimp();
+	void move(int speedMult);
+	void animate(int);
+	bool isDead();
+	void decreaseShotsToKill();
+	int scoreHit();
+	int scoreKilled();
+	private:
 };
 
 #endif
